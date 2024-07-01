d37b8ba19f36a7c08a72ca003501c1848e4bfd5c
Youssef Mohammad <153318445+DEVOLOPER-1@users.noreply.github.com>
2024-07-01 22:10:30 +0300

    removed logic for speeding and applied it into the notebook with savin the output into csv for easy readbaility

--- Dashboard.py
+++ Dashboard.py
@@ -35,45 +35,7 @@ count = 0
 with diff_date_bet_report_occurence_in_days:
     with st.container():
         st.write("col1")
-        date_reported = df["date_reported"].to_list()
-        print(date_reported[0])
-        date_occured = df["date_occured"].to_list()
-        print(date_occured[1])
 
-        cases_numbers = []
-        differences = []
-
-        for reported, occured in zip(date_reported, date_occured):
-            if reported != "nan" and occured != "nan":
-
-                try:
-                    date_time_obj_r = datetime.strptime(reported, "%m/%d/%Y %I:%M:%S %p")
-
-                    date_time_obj_o = datetime.strptime(occured, "%m/%d/%Y %I:%M:%S %p")
-
-                    year_diff = abs(date_time_obj_r.year - date_time_obj_o.year)
-
-                    month_diff = abs(date_time_obj_r.month - date_time_obj_o.month)
-
-                    day_diff = abs(date_time_obj_r.day - date_time_obj_o.day)
-
-                    total_diff_in_days = day_diff + (month_diff * 30) + (year_diff * 365)
-
-                    differences.append(total_diff_in_days)
-                    
-                    count+=1
-                except:
-                    count+=1
-                    print(f"bad format {count}")
-
-
-        for index, row in df.iterrows():
-            case_num = row["division_of_records"]
-            case_num = str(case_num)
-            case_num = case_num[4:9]
-            cases_numbers.append(case_num)
-            
-        diff_date_bet_report_occurence_in_days_df = pd.DataFrame({'x': cases_numbers, 'y': differences})
    
         st.line_chart( data= diff_date_bet_report_occurence_in_days_df , x='x', y='y', x_label='None', y_label='None',use_container_width=True)
 
