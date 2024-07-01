3ab9fdc102d6e5a93b4869cc972e15ac7a35c3ea
Youssef Mohammad <153318445+DEVOLOPER-1@users.noreply.github.com>
2024-07-01 19:57:53 +0300

    correct logic after testing and handling tons of errors

--- Dashboard.py
+++ Dashboard.py
@@ -5,8 +5,6 @@ import plotly.express as px
 from datetime import datetime
 
 
-
-
 st.set_page_config(
     page_title="Los Angeles Crimes",
     page_icon=":collision:",
@@ -32,46 +30,50 @@ st.header(body="Los Angeles Crimes :gun:", divider="red", anchor=False)
     mocodes_analysis,
 ) = st.columns(3, gap="medium")
 
+count = 0
+
 with diff_date_bet_report_occurence_in_days:
     with st.container():
         st.write("col1")
         date_reported = df["date_reported"].to_list()
         print(date_reported[0])
-        date_occured = df["date_occured"]
+        date_occured = df["date_occured"].to_list()
         print(date_occured[1])
-        division_of_records_list = df["division_of_records"].to_list()
-        print(division_of_records_list[0])
+
         cases_numbers = []
         differences = []
 
         for reported, occured in zip(date_reported, date_occured):
-            if reported != 'nan' and occured != 'nan':
+            if reported != "nan" and occured != "nan":
+
+                try:
+                    date_time_obj_r = datetime.strptime(reported, "%m/%d/%Y %I:%M:%S %p")
+
+                    date_time_obj_o = datetime.strptime(occured, "%m/%d/%Y %I:%M:%S %p")
+
+                    year_diff = abs(date_time_obj_r.year - date_time_obj_o.year)
+
+                    month_diff = abs(date_time_obj_r.month - date_time_obj_o.month)
+
+                    day_diff = abs(date_time_obj_r.day - date_time_obj_o.day)
+
+                    total_diff_in_days = day_diff + (month_diff * 30) + (year_diff * 365)
+
+                    differences.append(total_diff_in_days)
+                    
+                    count+=1
+                except:
+                    count+=1
+                    print(f"bad format {count}")
 
 
-                # Parse the date-time string to a datetime object
-                date_time_obj_r = datetime.strptime(reported, "%m/%d/%Y %I:%M:%S %p")
-                
-                date_time_obj_o = datetime.strptime(occured,"%m/%d/%Y %I:%M:%S %p")
+        for index, row in df.iterrows():
+            case_num = row["division_of_records"]
+            case_num = str(case_num)
+            case_num = case_num[4:9]
+            cases_numbers.append(case_num)
 
-                year_diff = abs(date_time_obj_r.year - date_time_obj_o.year)
-                
-                month_diff = abs(date_time_obj_r.month - date_time_obj_o.month)
-                
-                
-                day_diff = abs(date_time_obj_r.day - date_time_obj_o.day)
-                
-                total_diff_in_days = day_diff + (month_diff * 30) + (year_diff * 365)
-
-                differences.append(total_diff_in_days)
-
-
-                for value in division_of_records_list:
-                    if isinstance(value, float):
-                        print(f"Unexpected float value: {value}")
-                #print(type(value))
-                # case_num = int(value[4:9])
-                # cases_numbers.append(case_num)
-
+print(len(differences) , len(cases_numbers))
 
 with crime_code_with_description:
     with st.container():
