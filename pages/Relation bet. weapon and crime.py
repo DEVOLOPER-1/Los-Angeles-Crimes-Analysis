523dd26c361e62134646772b6e9220cf235c5cee
Youssef Mohammad <153318445+DEVOLOPER-1@users.noreply.github.com>
2024-07-02 22:33:47 +0300

    edited someting

--- pages/Relation bet. weapon and crime.py
+++ pages/Relation bet. weapon and crime.py
@@ -14,3 +14,31 @@ st.set_page_config(
         "Get help": "https://github.com/DEVOLOPER-1",
     },
 )
+
+
+df = pd.read_csv("weapon_code_df.csv")
+
+crime_weapon, weapons_count = st.columns(2, gap="medium")
+
+
+# Relation between the crime and weapon
+
+with crime_weapon:
+    with st.container():
+        st.scatter_chart(
+            data=df,
+            y="crime_code_identifier",
+            x="weapon_desc",
+            x_label="Commited Crime",
+            y_label="Used Weapon",
+        )
+
+
+
+weapons_count_dict = dict(df["weapon_desc"].value_counts())
+df["weapon_used_counts"] = df["weapon_desc"].map(weapons_count_dict)
+
+with weapons_count:
+    with st.container():
+        fig = px.pie(df, names="weapon_desc", values="weapon_used_counts")
+        st.plotly_chart(fig, use_container_width=True)
